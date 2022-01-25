bcdc5e6a5bdbb7835a1c79b86f38d5bc432025e4
volta <806269155@qq.com>
2022-01-25 11:17:42 +0800

    add cmd

---
+++ cmd/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "voltanet"
+
+func main()  {
+	net := voltanet.NewVoltaNet("0.0.0.0",19999,"json")
+	net.Router("/ping", func(ctx *voltanet.Context) {
+		ctx.Back()
+	})
+	net.Run()
+}
